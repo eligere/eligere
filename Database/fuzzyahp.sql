1c7bcc0b39fa8a7e99aecae1352443e9e733dc4d
Mateusz Gospodarczyk <MateuszGospodarczyk@gmail.com>
2016-07-18 01:06:02 +0200

    add lic
    lic

--- Database/fuzzyahp.sql
+++ Database/fuzzyahp.sql
@@ -6,6 +6,27 @@
 -- Generation Time: Mar 23, 2016 at 10:42 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
+
+--/**********************************************************************************************************
+-- *  <ELIGERE: a Fuzzy AHP Distributed Software Platform for Group Decision Making in Engineering Design>  *
+-- *   Copyright (C) 2016  by Mateusz Gospodarczyk and Stanislao Grazioso                                   *
+-- *  																									  *
+-- *   ELIGERE is free software: you can redistribute it and/or modify									  *
+-- *   it under the terms of the GNU General Public License as 											  *
+-- *   published by the Free Software Foundation, either version 3 of the 								  *
+-- *   License, or (at your option) any later version.													  *
+-- *																										  *
+-- *   This program is distributed in the hope that it will be useful,									  *
+-- *   but WITHOUT ANY WARRANTY; without even the implied warranty of										  *
+-- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the										  *
+-- *   GNU General Public License for more details.														  *
+-- *																										  *
+-- *   You should have received a copy of the GNU General Public License									  *
+-- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.								  *
+-- * 																										  *
+-- *   Contacts: mateusz.gospodarczyk@uniroma2.it and stanislao.grazioso@unina.it 						  *
+-- *********************************************************************************************************/
+
 use fuzzyahp;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
