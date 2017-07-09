c76b062a6c43300f96ed63f52cea52c580c9cf13
Mateusz Gospodarczyk <MateuszGospodarczyk@gmail.com>
2017-07-09 19:25:54 +0200

    delete files

--- WebApplication/dist/myfun.js
+++
@@ -1,8 +0,0 @@
-    var show = function(myDiv){
-      myDiv.style.display = "block";
-      //setTimeout(hide, 5000);  // 5 seconds
-    }
-
-    var hide = function(myDiv){
-      myDiv.style.display = "none";
-    }
